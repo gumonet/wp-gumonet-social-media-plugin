cad0e8550a1cf0320e7c98d3495e372bd64deba3
Angel Gutierrez <gutierrez.monola@gmail.com>
2022-04-27 16:43:50 -0500

    Initial project

--- includes/views/view-wp-gm-social-icons.php
+++ includes/views/view-wp-gm-social-icons.php
@@ -1,16 +1,16 @@
 <div class="wp-gm-social-icon-elements-container">
-	<div class="wp-gm-social-icon-elements-list">
-		<a class="wp-gm-social-icon-icon-link-container" href="#">
-			<span class="wp-gm-social-icon-icon-content wp-gm-social-icon-icon-facebook"></span>
-		</a>
-		<a class="wp-gm-social-icon-icon-link-container" href="#">
-			<span class="wp-gm-social-icon-icon-content wp-gm-social-icon-icon-instagram"></span>
-		</a>
-		<a class="wp-gm-social-icon-icon-link-container" href="#">
-			<span class="wp-gm-social-icon-icon-content wp-gm-social-icon-icon-linkedin"></span>
-		</a>
-		<a class="wp-gm-social-icon-icon-link-container" href="#">
-			<span class="wp-gm-social-icon-icon-content wp-gm-social-icon-icon-youtube"></span>
-		</a>
-	</div>
+    <div class="wp-gm-social-icon-elements-list">
+        <a class="wp-gm-social-icon-icon-link-container" href="https://www.facebook.com/desarrollodeagentes/" target="_blank">
+            <span class="wp-gm-social-icon-icon-content wp-gm-social-icon-icon-facebook"></span>
+        </a>
+        <a class="wp-gm-social-icon-icon-link-container" href="https://www.instagram.com/desarrollodeagentes/" target="_blank">
+            <span class="wp-gm-social-icon-icon-content wp-gm-social-icon-icon-instagram"></span>
+        </a>
+        <a class="wp-gm-social-icon-icon-link-container" href="https://www.linkedin.com/company/gyatussue%C3%B1os/" target="_blank">
+            <span class="wp-gm-social-icon-icon-content wp-gm-social-icon-icon-linkedin"></span>
+        </a>
+        <a class="wp-gm-social-icon-icon-link-container" href="https://www.youtube.com/channel/UCEn7ESBAQo08wPb4O1_jSRQ" target="_blank">
+            <span class="wp-gm-social-icon-icon-content wp-gm-social-icon-icon-youtube"></span>
+        </a>
+    </div>
 </div>
